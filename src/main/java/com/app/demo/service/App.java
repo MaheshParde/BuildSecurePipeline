83705fd360518945ddb8a15c3b0fc83604b9187a
LAPTOP-58L05II1\parde <pardemahesh9595@gmai.com>
2021-01-21 21:24:29 +0530

    latest code updated

--- src/main/java/com/app/demo/service/App.java
+++ src/main/java/com/app/demo/service/App.java
@@ -5,5 +5,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "App of java is started successfully..!!" );
+        System.out.println("Hii...users");
     }
 }
