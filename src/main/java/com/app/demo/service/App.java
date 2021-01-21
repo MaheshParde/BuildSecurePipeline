bd1c1cdcaef5d981868da6389bfe58840fb113e4
LAPTOP-58L05II1\parde <pardemahesh9595@gmai.com>
2021-01-21 16:49:52 +0530

    second java update

--- src/main/java/com/app/demo/service/App.java
+++ src/main/java/com/app/demo/service/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "App started successfully" );
+        System.out.println( "App started successfully..!!" );
     }
 }
